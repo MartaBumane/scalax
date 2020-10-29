44bb86ca0b875ae0661e0160937923cda7a596bc
Marta Bumane <mbumane2@evolutiongaming.com>
2020-10-29 19:50:18 +0200

    HW9

--- src/test/scala/com/github/t3hnar/scalax/util/ExpiringCacheSpec.scala
+++ src/test/scala/com/github/t3hnar/scalax/util/ExpiringCacheSpec.scala
@@ -8,49 +8,48 @@ import org.specs2.specification.Scope
  * @author Yaroslav Klymko
  */
 class ExpiringCacheSpec extends Specification {
-  "ExpiringCache" should {
-    "clean expired values if get enough queries" in new ExpiringCacheScope {
-      cache.map must haveSize(0)
-      cache.queryCount mustEqual 0
+    "ExpiringCache" should {
+        "clean expired values if get enough queries" in new ExpiringCacheScope {
+            cache.map must haveSize(0)
+            cache.queryCount mustEqual 0
 
-      cache.put(0, "0")
-      cache.get(0) must beSome("0")
-      cache.map must haveSize(1)
-      cache.queryCount mustEqual 1
+            cache.put(0, "0")
+            cache.get(0) must beSome("0")
+            cache.map must haveSize(1)
+            cache.queryCount mustEqual 1
 
-      current = cache.unit.toMillis(cache.duration)
+            current = cache.unit.toMillis(cache.duration)
 
-      cache.put(1, "1")
-      cache.get(1) must beSome("1")
-      cache.queryCount mustEqual 2
+            cache.put(1, "1")
+            cache.get(1) must beSome("1")
+            cache.queryCount mustEqual 2
 
-      (0 to cache.queryOverflow).foreach(_ => cache.get(3))
+            (0 to cache.queryOverflow).foreach(_ => cache.get(3))
 
-      cache.map.size must eventually(beEqualTo(1))
-      cache.get(1) must beSome("1")
+            cache.map.size must eventually(beEqualTo(1))
+            cache.get(1) must beSome("1")
+        }
+
+        "not return expired values which are not cleaned" in new ExpiringCacheScope {
+            cache.map must haveSize(0)
+            cache.queryCount mustEqual 0
+
+            cache.put(0, "0")
+            cache.get(0) must beSome("0")
+            cache.map.size must eventually(beEqualTo(1))
+
+            current = cache.unit.toMillis(cache.duration)
+
+            cache.get(0) must beNone
+            cache.map.size must eventually(beEqualTo(1))
+        }
+
     }
 
-    "not return expired values which are not cleaned" in new ExpiringCacheScope {
-      cache.map must haveSize(0)
-      cache.queryCount mustEqual 0
-
-      cache.put(0, "0")
-      cache.get(0) must beSome("0")
-      cache.map.size must eventually(beEqualTo(1))
-
-      current = cache.unit.toMillis(cache.duration)
-
-      cache.get(0) must beNone
-      cache.map.size must eventually(beEqualTo(1))
+    class ExpiringCacheScope extends Scope {
+        var current = 0L
+        val cache = new ExpiringCache[Int, String](1, TimeUnit.MILLISECONDS, 5) {
+            override def currentMillis = current
+        }
     }
-
-  }
-
-  class ExpiringCacheScope extends Scope {
-    var current = 0L
-    val cache = new ExpiringCache[Int, String](1, TimeUnit.MILLISECONDS, 5) {
-      override def currentMillis = current
-    }
-  }
 }
-
